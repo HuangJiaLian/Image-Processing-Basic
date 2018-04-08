8594302de34bc5fa01d7e784bc9a1d804e5718c9
Jack_Huang <719668276@qq.com>
2018-04-08 17:33:08 +0800

    add edge

---
+++ 04_Edge/01_sobel.c
@@ -0,0 +1,92 @@
+/*
+compile: gcc 01_sobel.c -lm -o sobel
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define MAX_LINE 10
+#define WIDTH 640
+#define HEIGHT 480
+
+int main(int argc, char const *argv[])
+{
+
+    FILE *fp;
+    int temp;
+    char buf[MAX_LINE];
+    int len;
+    int count=0;
+    int i, j;
+
+    unsigned char pixel[WIDTH*HEIGHT];
+    int Gx[WIDTH*HEIGHT];
+    int Gy[WIDTH*HEIGHT];
+    float G[WIDTH*HEIGHT];
+    float theta[WIDTH*HEIGHT];
+
+    if(argc != 2)
+    {
+        printf("Usage: %s filename\n",argv[0]);
+        exit(0);
+    }
+
+    fp = fopen(argv[1], "r");
+    while(fgets(buf,MAX_LINE,fp)!=NULL)
+    {   
+        len=strlen(buf);
+        buf[len-1]='\0';
+        temp=atoi(buf);
+
+        // Change char to unsigneed char 
+        if (temp < 0)
+        {
+            temp = 256 + temp;
+        }
+
+        pixel[i + j*WIDTH]=temp;
+        i++;
+        if(i==WIDTH)
+        {
+            i=0;
+            j++;
+            if(j==HEIGHT)
+            {
+                printf("Assignment compleated.\n");
+            }
+        }
+        count ++;
+    }
+    fclose(fp);
+
+    // Save the image.
+    stbi_write_png("Original_Image.png", WIDTH, HEIGHT, 1, pixel, 0);
+
+    for(j = 0; j < HEIGHT; j++)
+    {
+        for(i = 0; i < WIDTH; i++)
+        {
+            if( i == 0 || j == 0 || i == WIDTH - 1 || j == HEIGHT - 1)
+            {
+                Gx[i + j*WIDTH] = 0;
+                Gy[i + j*WIDTH] = 0;
+                G[i + j*WIDTH] = 0;
+            }else
+            {
+            // Gx = (-1)*pixel[i-1][j-1]  + pixel[i+1][j-1] + (-2)*pixel[i-1][j] + 2*pixel[i+1][j] + (-1)*pixel[i-1][j+1] + pixel[i+1][j+1];
+            // Gy = pixel[i-1][j-1] + 2*pixel[i][j-1] + pixel[i+1][j-1] + (-1)*pixel[i-1][j+1] + (-2)*pixel[i][j+1] + (-1)*pixel[i+1][j+1];
+                Gx[i + j*WIDTH] = (-1)*pixel[i-1 + (j-1)*WIDTH]  + pixel[i+1+(j-1)*WIDTH] + (-2)*pixel[i-1+j*WIDTH] + 2*pixel[i+1+j*WIDTH] + (-1)*pixel[i-1+(j+1)*WIDTH] + pixel[i+1+(j+1)*WIDTH];
+                // Gy[i + j*WIDTH] = pixel[i-1][j-1] + 2*pixel[i][j-1] + pixel[i+1][j-1] + (-1)*pixel[i-1][j+1] + (-2)*pixel[i][j+1] + (-1)*pixel[i+1][j+1];
+                // G[i + j*WIDTH] = 0;
+            }
+        }
+    }
+
+    // Save Gx
+    stbi_write_png("Gx.png", WIDTH, HEIGHT, 1, Gx, 0);
+    return 0;
+}
